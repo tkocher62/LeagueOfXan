58f274d52cda278aae4716d74e7746ee13eaa761
Cyanox62 <cyanox62@gmail.com>
2020-10-07 12:36:42 -0400

    Fixed demon and chort running animations

--- Assets/Scripts/Enemies/Chort/ChortController.cs
+++ Assets/Scripts/Enemies/Chort/ChortController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DemonController : Enemy
+public class ChortController : Enemy
 {
     public float movementSpeed;
     public float detectionRange;
