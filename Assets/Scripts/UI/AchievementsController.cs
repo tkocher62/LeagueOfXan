799bbffb2df23ef1822c6e7993e62596854d5a40
Cyanox62 <cyanox62@gmail.com>
2020-10-11 00:50:50 -0400

    Added achievements screen, not yet finished

---
+++ Assets/Scripts/UI/AchievementsController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+	class AchievementsController : MonoBehaviour
+	{
+		public void Back() => SceneManager.LoadScene(0);
+	}
+}
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,7 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame() => SceneManager.LoadScene(2);
+    public void PlayGame() => SceneManager.LoadScene(3);
+
+    public void AchievementsMenu() => SceneManager.LoadScene(2);
 
     public void OptionsMenu() => SceneManager.LoadScene(1);
 
