d876cf197f09189eea0df7a27a745125a6ba9582
Cyanox62 <cyanox62@gmail.com>
2020-11-13 15:45:45 -0500

    Finished master fireball logic

--- Assets/Scripts/Enemies/LeagueOfLegend/FireballController.cs
+++ Assets/Scripts/Enemies/LeagueOfLegend/FireballController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +8,16 @@ namespace Assets.Scripts.Enemies.LeagueOfLegend
 {
 	class FireballController : MonoBehaviour
 	{
+        private float distFromPlayer;
+        private float explodeDist;
+
+        private const float triggerDistance = 0.7f;
+
         private void Start()
         {
+            distFromPlayer = Vector2.Distance(gameObject.transform.position, PlayerController.singleton.gameObject.transform.position);
+            explodeDist = -1f;
+
             Rigidbody2D rb = gameObject.AddComponent<Rigidbody2D>();
             rb.gravityScale = 0f;
 
@@ -24,6 +31,20 @@ namespace Assets.Scripts.Enemies.LeagueOfLegend
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg - 180));
 
             rb.AddForce((PlayerController.singleton.transform.position - gameObject.transform.position).normalized * 500f);
+
+            explodeDist = Random.Range(0f, distFromPlayer * triggerDistance);
         }
-	}
+
+        private void Update()
+        {
+            float dist = Vector2.Distance(gameObject.transform.position, PlayerController.singleton.gameObject.transform.position);
+
+            float t = dist - explodeDist;
+            if (t < 0.1f && t > -0.1f)
+            {
+                // explode
+                Destroy(gameObject);
+            }
+        }
+    }
 }
